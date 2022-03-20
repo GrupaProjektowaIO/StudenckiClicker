82a16b8181bd2afad2cfe1b61ed0ae3d5a60d90b
Mateusz-Papis <101595888+Mateusz-Papis@users.noreply.github.com>
2022-03-20 21:47:58 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ pygame.display.set_caption("Studencki")
 pygame.font.Font("freesansbold.ttf", 16)
 timer = pygame.time.Clock()
 
+#fonty
+font = pygame.font.SysFont('Comic Sans MS, Arial, Times New Roman', 16, bold=True, italic=True)
+text = font.render("Zadania", True, (255, 255, 255))
+
 board = pygame.image.load("sprites/board.png")
 
 healthbar = pygame.image.load("sprites/health_bar.png")
@@ -25,20 +29,103 @@ sanityicon = pygame.image.load("sprites/sanity_icon.png")
 timeicon = pygame.image.load("sprites/time_icon.png")
 
 objective_panel = pygame.image.load("sprites/objective_panel.png")
+objective_panel_reversed = pygame.image.load("sprites/objective_panel_reversed.png")
+crystal_white = pygame.image.load("sprites/crystal_white.png")
+crystal_black = pygame.image.load("sprites/crystal_black.png")
+crystal_red = pygame.image.load("sprites/crystal_red.png")
+crystal_green = pygame.image.load("sprites/crystal_green.png")
+crystal_blue = pygame.image.load("sprites/crystal_blue.png")
+
 party = pygame.image.load("sprites/objective_icons/party.png")
 
-objective_panel_arrow = pygame.image.load("sprites/objective_panel_arrow.png")
+#dzwiek
+clickSound = pygame.mixer.Sound('click.wav')
+winclickSound = pygame.mixer.Sound('winclick.wav')
+midclickSound = pygame.mixer.Sound('midclick.wav')
+
+
+#kolory
+red = (255, 0, 0)
+blue = (0, 0, 255)
+green = (0, 255, 0)
+
+
+class Objective:
+    def __init__(self):
+        self.points = 0
+        self.pointsToComplete = 100
+        self.crystalType = 0
+        self.addHealth = 0
+        self.addSanity = 0
+        self.addTime = 0
+
+class ObjectiveType:
+    def __init__(self, title, desc):
+        self.title = title
+        self.desc = desc
+
+objectiveTypes = {}
+objectiveTypes["party"] = ObjectiveType("Impreza!", "Czas na małą imprezkę")
+objecives =\
+[
+    Objective(), Objective(), Objective(),
+    Objective(), Objective(), Objective()
+]
+
+objecives[0].crystalType = 2
+objecives[1].crystalType = 1
+objecives[2].crystalType = 0
+objecives[3].crystalType = 2
+objecives[4].crystalType = 1
+objecives[5].crystalType = 0
+objecives[0].addHealth = 10
+objecives[0].addSanity = -5
+objecives[0].addTime = 5
+
+
 
 def centerAnchor(width, height, percent_x=0.5, percent_y=0.5,
                  offset_x=0, offset_y=0):
     return pygame.Rect(screen.get_width() * percent_x - width / 2 + offset_x,
                        screen.get_height() * percent_y - height / 2 + offset_y,
                        width, height)
+def renderObjectivePanel(percent_y=0.5, offset_x=0, index=0, reversed=False):
+    panel = objective_panel
+    if reversed:
+        panel = objective_panel_reversed
+    screen.blit(panel, centerAnchor(231, 123, reversed, percent_y, (1 - reversed * 2) * (231 / 2) * offset_x))
+
+    panel.blit(text,(panel.get_width() / 2 - text.get_rect().width / 2, panel.get_height() / 2 - text.get_rect().height / 2))
+    if objecives[index].crystalType == 0:
+        screen.blit(crystal_red, centerAnchor(25, 25, reversed, percent_y, (1 - reversed * 2) * (231 - (231 / 2) * (1 - offset_x))))
+    elif objecives[index].crystalType == 1:
+        screen.blit(crystal_green, centerAnchor(25, 25, reversed, percent_y, (1 - reversed * 2) * (231 - (231 / 2) * (1 - offset_x))))
+    else:
+        screen.blit(crystal_blue, centerAnchor(25, 25, reversed, percent_y, (1 - reversed * 2) * (231 - (231 / 2) * (1 - offset_x))))
+
+    if objecives[index].addHealth > 0:
+        screen.blit(crystal_white, centerAnchor(14, 14, reversed, percent_y, (1 - reversed * 2) * (248 - (231 / 2) * (1 - offset_x)), -21))
+    elif objecives[index].addHealth < 0:
+        screen.blit(crystal_black, centerAnchor(14, 14, reversed, percent_y, (1 - reversed * 2) * (248 - (231 / 2) * (1 - offset_x)), -21))
+
+    if objecives[index].addSanity > 0:
+        screen.blit(crystal_white, centerAnchor(14, 14, reversed, percent_y, (1 - reversed * 2) * (253 - (231 / 2) * (1 - offset_x))))
+    elif objecives[index].addSanity < 0:
+        screen.blit(crystal_black, centerAnchor(14, 14, reversed, percent_y, (1 - reversed * 2) * (253 - (231 / 2) * (1 - offset_x))))
+
+    if objecives[index].addTime > 0:
+        screen.blit(crystal_white, centerAnchor(14, 14, reversed, percent_y, (1 - reversed * 2) * (248 - (231 / 2) * (1 - offset_x)), 21))
+    elif objecives[index].addTime < 0:
+        screen.blit(crystal_black, centerAnchor(14, 14, reversed, percent_y, (1 - reversed * 2) * (248 - (231 / 2) * (1 - offset_x)), 21))
 
 running = True
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            winclickSound.play()
+            draw_green = True
+            pos = pygame.mouse.get_pos()
+        elif event.type == pygame.QUIT:
             running = False
 
     screen.fill((0,0,0))
@@ -50,16 +137,14 @@ while running:
     screen.blit(sanityicon, centerAnchor(57, 58, 0.5, 0, 0, 256 // 2 + 20))
     screen.blit(timeicon, centerAnchor(49, 56, 0.5, 0, 0, 256 // 2 + 20 + 70))
 
-    #screen.blit(objective_panel, centerAnchor(207, 128, 0, 0.25, 270/2 + 10))
-    #screen.blit(party, centerAnchor(32, 32, 0, 0.25, 270/2 + 10, -64))
-    screen.blit(objective_panel_arrow, centerAnchor(778, 466, 0, 0.25, 778 / 2 + 10))
+    renderObjectivePanel(0.25, 1, 0) # od -1.5 do 1
+    renderObjectivePanel(0.5, 1, 1)
+    renderObjectivePanel(0.75, 1, 2)
 
-    screen.blit(objective_panel, centerAnchor(207, 128, 0, 0.5, 270 / 2 + 10))
-    screen.blit(objective_panel, centerAnchor(207, 128, 0, 0.75, 270 / 2 + 10))
+    renderObjectivePanel(0.25, 1, 3, True)
+    renderObjectivePanel(0.5, 1, 4, True)
+    renderObjectivePanel(0.75, 1, 5, True)
 
-    screen.blit(objective_panel, centerAnchor(207, 128, 1, 0.25, -(270 / 2 + 10)))
-    screen.blit(objective_panel, centerAnchor(207, 128, 1, 0.5, -(270 / 2 + 10)))
-    screen.blit(objective_panel, centerAnchor(207, 128, 1, 0.75, -(270 / 2 + 10)))
 
     pygame.display.flip()
 pygame.quit()
